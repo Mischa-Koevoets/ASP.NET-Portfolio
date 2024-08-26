962ddf19d72d201eb2eeeff960ce2544941b0667
Mischa Koevoets <mischakoevoetssch@gmail.com>
2024-08-26 11:42:09 +0200

    initial commit

---
+++ aspnetPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnetPortfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
